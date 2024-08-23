d4414f68616a81ebfd63581d21766108caf031f3
lucabeca <lucas.17aac16@gmail.com>
2024-08-23 10:07:50 -0300

    inicial

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
